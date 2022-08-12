3bc0361f4ee1d3538f0f6870e4c60e70af400cdc
SuperDiamondHD <39006467+SuperDiamondHD@users.noreply.github.com>
2022-08-12 13:25:46 +0200

    Minor improvements
    Removed unnecessary code, logs are shown on website only for server index 0

--- toolkit.py
+++ toolkit.py
@@ -1,13 +1,9 @@
-from xml.dom import UserDataHandler
 import rcon
 import json
+import time
 from file_read_backwards import FileReadBackwards
 
-
-# file = open('userdata.json')
-# userdata = json.load(file)
-# file.close()
-
+# USERDATA FUNCTIONS
 def get_userdata():
     file = open('userdata.json')
     userdata = json.load(file)
@@ -18,13 +14,13 @@ def get_userdata():
 # CONSTANTS
 CONFIG = get_userdata()["configuration"]
 LOG_READ_LIMIT = CONFIG["log-read-limit"]
+LOG_COUNT_WAIT = CONFIG["log-count-wait"]
 
-# USERDATA FUNCTIONS
 def get_server_path(server_index):
     servers = get_userdata()["servers"]
     path = servers[server_index]["path"]
     if path == "default":
-        path = servers[server_index]["servername"]
+        path = "servers/" + servers[server_index]["servername"]
 
     return path
 
@@ -39,7 +35,7 @@ def set_rcon(host, port, password):
     rcon_password = password
 
 def run_command(*args):
-    with rcon.Client(RCON_HOST, RCON_PORT, passwd=RCON_PASSWORD) as client:
+    with rcon.Client(rcon_host, rcon_port, passwd=rcon_password) as client:
         client.run(args)
 
 # GET LOGS FUNCTIONS
@@ -48,7 +44,7 @@ def get_latest_logs(server_index, limit=LOG_READ_LIMIT):
 
     path = get_server_path(server_index)
     
-    with FileReadBackwards("servers/" + path + "/logs/latest.log", encoding="utf-8") as file:
+    with FileReadBackwards(path + "/logs/latest.log", encoding="utf-8") as file:
         index = 0
         for line in file:
             if index >= limit:
@@ -58,28 +54,22 @@ def get_latest_logs(server_index, limit=LOG_READ_LIMIT):
 
     return logs[::-1]
 
-############
-# OLD CODE #
-############
+def get_loglines_count(server_index):
+    file = open(get_server_path(server_index) + "/logs/latest.log")
+    line_count = 0
+    buffer_size = 1024 * 1024
+    read_file = file.read
 
-# def find_line_at_index(filename, index):
-#     file = open(filename)
-#     for i, line in enumerate(file):
-#         if i == index - 1: 
-#             return line 
-#     file.close()
-# while True: 
-    #     if sum(1 for i in open(SERVER_FOLDER + "\\logs\\latest.log", "rb")) >= line_index:
-    #         print(find_line_at_index(SERVER_FOLDER + "\\logs\\latest.log", line_index))
-    #         line_index = line_index + 1
-    #     elif sum(1 for i in open(SERVER_FOLDER + "\\logs\\latest.log", "rb")) + 1 < line_index:
-    #         line_index = 1
-
-    # index = 0
-    # a = "Hello World!\r\nThis is a new line\r\nalso new line\r\n"
-    # for logline in open("servers/"+ path + "/logs/latest.log", "r"):
-    #     index -= 1
-
-    #     logs.append(logline[index])
+    buffer = read_file(buffer_size)
+    while buffer:
+        line_count += buffer.count("\n")
+        buffer = read_file(buffer_size)
     
-    # with open("servers/" + path + "/logs
+    return line_count
+
+def wait_new_logline(server_index):
+    line_count = get_loglines_count(server_index)
+    new_line_count = line_count
+    while line_count == new_line_count:
+        new_line_count = get_loglines_count(server_index)
+        time.sleep(LOG_COUNT_WAIT)
